ebc19931fcc2d57c343ed2d2bd8ab6d86d7d0950
jilliankmartin <jillmartin@live.com>
2020-11-10 22:59:48 +0000

    added sold out badge

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -3,6 +3,7 @@ class CategoriesController < ApplicationController
   def show
     @category = Category.find(params[:id])
     @products = @category.products.order(created_at: :desc)
+    raise @products.inspect
   end
 
 end
