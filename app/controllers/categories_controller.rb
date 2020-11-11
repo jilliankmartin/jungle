784666d14f3453662c654713255516cf9b394902
jilliankmartin <jillmartin@live.com>
2020-11-11 04:06:47 +0000

    page is displaying - order confirmation

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -3,7 +3,6 @@ class CategoriesController < ApplicationController
   def show
     @category = Category.find(params[:id])
     @products = @category.products.order(created_at: :desc)
-    raise @products.inspect
   end
 
 end
